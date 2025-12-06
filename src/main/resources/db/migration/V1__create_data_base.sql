1e0c79cbe9d5332cc223602521d6570840adbdc7
Elzat-developer <fergenson.2020@gmail.com>
2025-12-06 19:50:50 +0500

    Изменил

--- src/main/resources/db/migration/V1__create_data_base.sql
+++ src/main/resources/db/migration/V1__create_data_base.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `users` (
                                        street varchar(255) DEFAULT NULL,
                                        house_or_apartment varchar(255) DEFAULT NULL,
                                        index_post varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS products (
                                         id int auto_increment primary key,
                                         name varchar(255) DEFAULT NULL,
@@ -20,11 +20,11 @@ CREATE TABLE IF NOT EXISTS products (
                                         price int DEFAULT NULL,
                                         old_price int DEFAULT NULL,
                                         catalogs_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS catalogs (
                                         id int auto_increment primary key,
                                         name varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS companies (
                                         id int auto_increment primary key,
                                         name varchar(255) DEFAULT NULL,
@@ -32,13 +32,13 @@ CREATE TABLE IF NOT EXISTS companies (
                                         photo_url varchar(255) DEFAULT NULL,
                                         base varchar(255) DEFAULT NULL,
                                         city varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS product_photos (
                                          id int auto_increment primary key,
                                          photo_url varchar(255) DEFAULT NULL,
                                          products_id int DEFAULT NULL,
                                          promotions_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS promotions (
                                          id int auto_increment primary key,
                                          name varchar(255) DEFAULT NULL,
@@ -50,31 +50,31 @@ CREATE TABLE IF NOT EXISTS promotions (
                                          end_date varchar(255) DEFAULT NULL,
                                          catalogs_id int DEFAULT NULL,
                                          products_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS orders (
                                           id int auto_increment primary key,
                                           name varchar(255) DEFAULT NULL,
                                           order_start_date datetime DEFAULT NULL,
                                           paid_status enum('PAID','NOTPAY') DEFAULT 'NOTPAY',
                                           users_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS order_products (
                                           orders_id int auto_increment primary key,
                                           products_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS order_items (
                                       id int auto_increment primary key,
                                       quantity int DEFAULT NULL,
                                       products_id int DEFAULT NULL,
                                       orders_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS carts (
                                       id int auto_increment primary key,
                                       users_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 CREATE TABLE IF NOT EXISTS cart_items (
                                       id int auto_increment primary key,
                                       quantity int DEFAULT NULL,
                                       carts_id int DEFAULT NULL,
                                       products_id int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
