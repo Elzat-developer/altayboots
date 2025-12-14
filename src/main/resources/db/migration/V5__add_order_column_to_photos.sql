1a26659041c01c373512cbdaabe288ececbe9501
Elzat-developer <fergenson.2020@gmail.com>
2025-12-14 12:34:39 +0500

    Миграция исправлена

--- src/main/resources/db/migration/V5__add_order_column_to_photos.sql
+++ src/main/resources/db/migration/V5__add_order_column_to_photos.sql
@@ -1,6 +1,12 @@
+DROP INDEX idx_product_photos_promo_order ON product_photos;
+DROP INDEX idx_product_photos_product_order ON product_photos;
+
+ALTER TABLE product_photos
+    DROP COLUMN photo_order_index;
+
 ALTER TABLE product_photos
     ADD COLUMN photo_order_index INT NULL DEFAULT 0;
 -- Создаем индекс для более быстрого доступа к порядку, если необходимо (опционально)
-CREATE INDEX idx_product_photos_order ON product_photos (promotions_id, photo_order);
+CREATE INDEX idx_product_photos_order ON product_photos (promotions_id, photo_order_index);
 -- Добавление нового индекса для оптимизации выборки по Product
-CREATE INDEX idx_product_photos_product_order ON product_photos (products_id, photo_order);
+CREATE INDEX idx_product_photos_product_order ON product_photos (products_id, photo_order_index);
